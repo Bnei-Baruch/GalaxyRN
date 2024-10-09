66d6260813f6c1e9976f38993db665c85749ad48
davgur <davgur.ru@gmail.com>
2024-10-09 11:50:42 +0300

    hide switch devicecs

--- android/app/src/main/java/com/galaxyrn/MainActivity.kt
+++ android/app/src/main/java/com/galaxyrn/MainActivity.kt
@@ -19,4 +19,14 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+/*
+        @Override
+         protected List<ReactPackage> getPackages() {
+             @SuppressWarnings("UnnecessaryLocalVariable")
+             List<ReactPackage> packages = new PackageList(this).getPackages();
+             // Packages that cannot be autolinked yet can be added manually here, for example:
+             packages.add(new MyReactPackage()); // Replace 'MyReactPackage' with your package that contains the module
+             return packages;
+         }
+         */
 }
