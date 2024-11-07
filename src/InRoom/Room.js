3e6bec818dd75b837b3ad099deed620c4fa70928
davgur <davgur.ru@gmail.com>
2024-11-07 14:29:07 +0200

    scroll members

--- src/InRoom/Room.js
+++ src/InRoom/Room.js
@@ -1,20 +1,17 @@
 import { useEffect } from 'react';
 import { useInRoomStore } from '../zustand/inRoom';
 import { StyleSheet, ScrollView, View, TouchableWithoutFeedback } from 'react-native';
-import Member from './Member';
 import { ChatModal } from '../chat/ChatModal';
 import { Shidur } from '../shidur/Shidur';
-import { memberItemWidth } from './helper';
 import { useInitsStore } from '../zustand/inits';
-import { useSettingsStore } from '../zustand/settings';
-import MemberNoVideo from './MemberNoVideo';
 import { BottomBar } from '../bottomBar/BottomBar';
 import { TopBar } from '../topBar/TopBar';
+import { baseStyles } from '../constants';
+import Members from './Members';
 
 const Room = () => {
-  const { joinRoom, exitRoom, memberByFeed, activatePage, setShowBars } = useInRoomStore();
-  const { isPortrait }                                                  = useInitsStore();
-  const { audioMode }                                                   = useSettingsStore();
+  const { joinRoom, exitRoom, setShowBars } = useInRoomStore();
+  const { isPortrait }                      = useInitsStore();
 
   useEffect(() => {
     joinRoom();
@@ -23,55 +20,36 @@ const Room = () => {
     };
   }, []);
 
-  const handleScrollEnd = e => {
-    const contentOffsetX = e.nativeEvent.contentOffset.x;
-    activatePage(Math.round(contentOffsetX / memberItemWidth.get()));
-  };
-  const handleAnyPress  = () => {
+  const handleAnyPress = () => {
     setShowBars(true);
     setTimeout(() => setShowBars(false), 1000);
   };
 
   return (
-    <TouchableWithoutFeedback onPress={handleAnyPress}>
-      <View style={styles.container}>
-        <ChatModal />
-        <TopBar />
+    <View style={styles.container}>
+      <ChatModal />
+      <TopBar />
 
-        <View style={[styles.orientation, isPortrait ? styles.portrait : styles.landscape]}>
-          <View style={isPortrait ? styles.shidurPortrait : styles.shidurLandscape}>
-            <Shidur />
-          </View>
-
-          <ScrollView
-            disableScrollViewPanResponder={true}
-            snapToInterval={memberItemWidth.get()}
-            showsHorizontalScrollIndicator={false}
-            onMomentumScrollEnd={handleScrollEnd}
-            //contentContainerStyle={styles.roomsContainer}
-          >
-            <View style={styles.roomsContainer}>
-
-              {/*<MyRoomMedia />*/}
-              {
-
-                Object
-                  .values(memberByFeed)
-                  .map(m => (
-                    !audioMode ?
-                      <Member key={m.id} member={m} />
-                      : <MemberNoVideo key={m.id} member={m} />
-                  ))
-
-              }
+      <View style={[styles.orientation, isPortrait ? styles.portrait : styles.landscape]}>
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          style={[baseStyles.full, { backgroundColor: 'blue' }]}
+        >
+          <TouchableWithoutFeedback onPress={handleAnyPress} style={[baseStyles.full, {
+            backgroundColor: 'orange',
+            height         : '100%'
+          }]}>
+            <View style={[baseStyles.full, { backgroundColor: 'green' }]}>
+              <View style={isPortrait ? styles.shidurPortrait : styles.shidurLandscape}>
+                <Shidur />
+              </View>
+              <Members />
             </View>
-          </ScrollView>
-
-        </View>
-
-        <BottomBar />
+          </TouchableWithoutFeedback>
+        </ScrollView>
       </View>
-    </TouchableWithoutFeedback>
+      <BottomBar />
+    </View>
   );
 };
 export default Room;
@@ -82,18 +60,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     padding        : 10
   },
-  stickyHeader   : {
-    flexDirection: 'column'
-  },
-  roomsContainer : {
-    flex          : 1,
-    flexDirection : 'row',
-    flexWrap      : 'wrap',
-    justifyContent: 'space-around',
-  },
   orientation    : {
-    flex    : 1,
-    position: 'relative'
+    flex           : 1,
+    position       : 'relative',
   },
   portrait       : { flexDirection: 'column' },
   landscape      : { flexDirection: 'row' },
