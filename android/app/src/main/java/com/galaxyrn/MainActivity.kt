02c747b5f6c6e6019c89fe56d3d6c5f2b89b698e
davgur <davgur.ru@gmail.com>
2024-12-01 12:16:31 +0200

    remove custom bridge code

--- android/app/src/main/java/com/galaxyrn/MainActivity.kt
+++ android/app/src/main/java/com/galaxyrn/MainActivity.kt
@@ -1,59 +1,52 @@
 package com.galaxyrn
 
-import android.bluetooth.BluetoothDevice
-import android.content.IntentFilter
-import android.media.AudioManager
 import android.os.Bundle
 import android.util.Log
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.oney.WebRTCModule.WebRTCModuleOptions
 
 class MainActivity : ReactActivity() {
-
-    //private var audioDeviceChangeReceiver = AudioDeviceChangeReceiver()
 
     /**
      * Returns the name of the main component registered from JavaScript. This is used to schedule
      * rendering of the component.
      */
     override fun getMainComponentName(): String = "GalaxyRN"
-    /*
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(null)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
 
-            Log.i("GalaxyRN custom: MainActivity", "MainActivity onCreate")
-
-            val intentFilter = IntentFilter()
-            //intentFilter.addAction(AudioManager.ACTION_AUDIO_BECOMING_NOISY)
-            intentFilter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED)
-            intentFilter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)
-
-            registerReceiver(audioDeviceChangeReceiver, intentFilter)
-        }
+        Log.i("GalaxyRN custom: MainActivity", "MainActivity onCreate")
 
 
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(audioDeviceChangeReceiver)
+        val options = WebRTCModuleOptions.getInstance()
+        options.enableMediaProjectionService = true
     }
+
+    /*
+        override fun onDestroy() {
+            super.onDestroy()
+        }
     */
+
     /**
      * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
      * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
      */
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-/*
+    /*
         @Override
-         protected List<ReactPackage> getPackages() {
-             @SuppressWarnings("UnnecessaryLocalVariable")
-             List<ReactPackage> packages = new PackageList(this).getPackages();
-             // Packages that cannot be autolinked yet can be added manually here, for example:
-             packages.add(new MyReactPackage()); // Replace 'MyReactPackage' with your package that contains the module
-             return packages;
-         }
-         */
+        protected List<ReactPackage> getPackages()
+        {
+            @SuppressWarnings("UnnecessaryLocalVariable")
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            packages.add(new MyReactPackage ()); // Replace 'MyReactPackage' with your package that contains the module
+            return packages;
+        }
+    */
 }
--- android/app/src/main/java/com/galaxyrn/MainApplication.kt
+++ android/app/src/main/java/com/galaxyrn/MainApplication.kt
@@ -10,15 +10,14 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.oney.WebRTCModule.WebRTCModuleOptions
+
 
 class MainApplication : Application(), ReactApplication {
 
     override val reactNativeHost: ReactNativeHost =
         object : DefaultReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> =
-                PackageList(this).packages.apply {
-                    //add(GxyRNPackage())
-                }
+            override fun getPackages(): List<ReactPackage> = PackageList(this).packages.apply { }
 
             override fun getJSMainModuleName(): String = "index"
 
@@ -34,6 +33,7 @@ class MainApplication : Application(), ReactApplication {
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this, false)
+
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
             load()
