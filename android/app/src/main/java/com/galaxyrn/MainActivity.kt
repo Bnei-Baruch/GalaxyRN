d23d3760409af79d76c9e43df3c5050ffc718923
davgur <davgur.ru@gmail.com>
2024-12-30 12:54:16 +0200

    foreground

--- android/app/src/main/java/com/galaxyrn/MainActivity.kt
+++ android/app/src/main/java/com/galaxyrn/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.galaxyrn
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.oney.WebRTCModule.WebRTCModuleOptions
+
+
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +23,10 @@ class MainActivity : ReactActivity() {
      */
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val options = WebRTCModuleOptions.getInstance()
+        options.enableMediaProjectionService = true
+    }
 }
