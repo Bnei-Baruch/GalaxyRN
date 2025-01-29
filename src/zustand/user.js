5d3e87e6385fdb211ea8c76a3cf20eb047a16910
davgur <davgur.ru@gmail.com>
2025-01-29 17:59:02 +0200

    prepare for deploy

--- src/zustand/user.js
+++ src/zustand/user.js
@@ -12,10 +12,10 @@ export const useUserStore = create((set, get) => ({
   rfid         : null,
   setRfid      : (rfid) => set({ rfid }),
   sendUserState: (opts = {}, adminOpts = {}) => {
-    const { room: { room, description: group, janus } = {} } = useRoomStore.getState();
-    const { question }                                       = useSettingsStore.getState();
-    const { cammute }                                        = useMyStreamStore.getState();
-    const { vhinfo, user, rfid }                             = get();
+    const { room, description: group, janus } = useRoomStore.getState()?.room || {};
+    const { question }                        = useSettingsStore.getState();
+    const { cammute }                         = useMyStreamStore.getState();
+    const { vhinfo, user, rfid }              = get();
 
     if (!room) {
       console.log('sendUserState no room', room);
@@ -32,27 +32,9 @@ export const useUserStore = create((set, get) => ({
     const msg = { type: 'client-state', user: { ...defaultOpts, ...opts } };
     console.log('sendUserState room message', msg);
     mqtt.send(JSON.stringify(msg), false, 'galaxy/room/' + room);
-    const extra    = {
-      'streams': [
-        {
-          'type'        : 'video',
-          'mindex'      : 0,
-          'mid'         : '0',
-          'codec'       : 'h264',
-          'h264_profile': '42e01f'
-        },
-        {
-          'type'  : 'audio',
-          'mindex': 1,
-          'mid'   : '1',
-          'codec' : 'opus',
-          'fec'   : true
-        }
-      ],
-      'isGroup': false
-    };
-    const adminMsg = { ...user, ...msg.user, group, extra, vhinfo, janus, ...adminOpts };
+
+    const adminMsg = { ...user, ...msg.user, group, vhinfo, janus, ...adminOpts };
     console.log('sendUserState gxydb message', adminMsg);
-    mqtt.send(JSON.stringify(msg), false, 'gxydb/users');
+    adminOpts?.timestamp && mqtt.send(JSON.stringify(adminMsg), false, 'gxydb/users');
   }
 }));
