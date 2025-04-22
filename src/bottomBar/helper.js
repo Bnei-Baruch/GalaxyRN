4d11e9338c54d29edc47decde179c1fa93c5bff6
davgur <davgur.ru@gmail.com>
2025-04-22 11:06:09 +0300

    update bottom bar button styles to be responsive based on device dimensions

--- src/bottomBar/helper.js
+++ src/bottomBar/helper.js
@@ -1,16 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { topMenuBtns } from '../topBar/helper';
 import { baseStyles } from '../constants';
+
+const { width, height } = Dimensions.get('window');
+const buttonSize = Math.min(Math.min(width, height) /5, 75);
 
 export const bottomBar = StyleSheet.create({
   btn       : {
     alignItems     : 'center',
     justifyContent : 'center',
-    width          : 75,
-    height         : 75,
-    borderRadius   : 100,
-    marginRight    : 2,
-    marginLeft     : 2,
+    width          : buttonSize,
+    height         : buttonSize,
+    borderRadius   : buttonSize / 2,
     backgroundColor: 'rgba(34, 34, 34, 1)'
   },
   moreSelBtn: {
