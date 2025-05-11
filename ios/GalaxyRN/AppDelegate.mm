7e105b9da4538b9233dcfc1033c1d02e8aa2b1bf
davgur <davgur.ru@gmail.com>
2025-05-11 09:41:19 +0300

    sentry

--- ios/GalaxyRN/AppDelegate.mm
+++ ios/GalaxyRN/AppDelegate.mm
@@ -3,6 +3,8 @@
 #import <React/RCTBundleURLProvider.h>
 // Import Crisp SDK header
 #import <Crisp/Crisp-Swift.h>
+// Import Sentry
+#import <Sentry/Sentry.h>
 
 @implementation AppDelegate
 - (BOOL)application: (UIApplication *)application
@@ -20,6 +22,32 @@
   self.moduleName = @"GalaxyRN";
   self.initialProps = @{};
 
+  
+  // Initialize Sentry
+  NSString *sentryDSN = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"SENTRY_DSN"];
+  if (sentryDSN) {
+    [SentrySDK startWithConfigureOptions:^(SentryOptions *options) {
+      options.dsn = sentryDSN;
+      options.debug = NO;
+      options.tracesSampleRate = @0.2;
+      options.profilesSampleRate = @0.1;
+      options.enableAutoSessionTracking = YES;
+      options.attachScreenshot = YES;
+      options.attachViewHierarchy = YES;
+      
+      // Set environment
+      #if DEBUG
+      options.environment = @"development";
+      #else
+      options.environment = @"production";
+      #endif
+      
+      // Get app version for release name
+      NSString *version = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+      NSString *build = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"];
+      options.releaseName = [NSString stringWithFormat:@"GalaxyRN@%@+%@", version, build];
+    }];
+  }
   // Initialize Crisp SDK with website ID - move to background thread
   dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
     NSString *websiteID = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CRISP_WEBSITE_ID"];
--- src/sentryHelper.js
+++ src/sentryHelper.js
@@ -6,3 +6,82 @@ export const sendSentry = (msg, level = 'info') => {
     return;
   Sentry.captureMessage(msg, { level });
 };
+
+/**
+ * Capture an exception and send to Sentry
+ * @param {Error} error - The error to capture
+ * @param {Object} extraData - Additional data to include
+ */
+export const captureException = (error, extraData = {}) => {
+  if (!__DEV__) {
+    Sentry.withScope((scope) => {
+      if (extraData) {
+        Object.keys(extraData).forEach(key => {
+          scope.setExtra(key, extraData[key]);
+        });
+      }
+      Sentry.captureException(error);
+    });
+  } else {
+    console.error(error);
+  }
+};
+
+/**
+ * Set user information for Sentry tracking
+ * @param {Object} user - User information
+ */
+export const setUser = (user) => {
+  if (!user || !user.id) return;
+  
+  Sentry.setUser({
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  });
+};
+
+/**
+ * Clear user information when user logs out
+ */
+export const clearUser = () => {
+  Sentry.setUser(null);
+};
+
+/**
+ * Add breadcrumb to Sentry
+ * @param {string} category - Breadcrumb category
+ * @param {string} message - Breadcrumb message
+ * @param {Object} data - Additional data
+ * @param {string} level - Severity level
+ */
+export const addBreadcrumb = (category, message, data = {}, level = 'info') => {
+  Sentry.addBreadcrumb({
+    category,
+    message,
+    data,
+    level,
+  });
+};
+
+/**
+ * Set tag value for current scope
+ * @param {string} key - Tag key
+ * @param {string} value - Tag value
+ */
+export const setTag = (key, value) => {
+  Sentry.setTag(key, value);
+};
+
+/**
+ * Create transaction for performance monitoring
+ * @param {string} name - Transaction name
+ * @param {string} op - Operation name
+ * @returns {Transaction} - Sentry transaction
+ */
+export const startTransaction = (name, op) => {
+  return Sentry.startTransaction({
+    name,
+    op,
+  });
+};
