aba0a87404b986da59fce222a32d6057e9c77581
davgur <davgur.ru@gmail.com>
2025-05-11 12:19:14 +0300

    Refactor MyRoomVideo and MyVideo components to improve code readability and maintainability; replace overlay logic with CammutedFeed component and adjust icon size dynamically based on screen dimensions.

--- src/InRoom/Feeds/MyRoomVideo.js
+++ src/InRoom/Feeds/MyRoomVideo.js
@@ -1,43 +1,36 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import MyRTCView from '../../components/MyRTCView';
-import { useMyStreamStore } from '../../zustand/myStream';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import { useSettingsStore } from '../../zustand/settings';
-import FeedDisplay from './FeedDisplay';
-import { useUserStore } from '../../zustand/user';
-import { useUiActions } from '../../zustand/uiActions';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import MyRTCView from "../../components/MyRTCView";
+import { useMyStreamStore } from "../../zustand/myStream";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { useSettingsStore } from "../../zustand/settings";
+import FeedDisplay from "./FeedDisplay";
+import { useUserStore } from "../../zustand/user";
+import { useUiActions } from "../../zustand/uiActions";
+import CammutedFeed from "./CammutedFeed";
 
 const MyRoomMedia = () => {
-  const { cammute }  = useMyStreamStore();
+  const { cammute } = useMyStreamStore();
   const { question } = useSettingsStore();
-  const { user }     = useUserStore();
-  const { width }    = useUiActions();
+  const { user } = useUserStore();
+  const { width } = useUiActions();
 
   return (
     <View style={{ width }}>
-      <FeedDisplay display={user.username} />
+      {(!cammute) && <FeedDisplay display={user.username} />}
       <View style={styles.content}>
-        {
-          cammute ? (
-            <View style={styles.overlay}>
-              <Icon name="account-circle" size={80} color="white" />
-            </View>
-          ) : <MyRTCView />
-        }
+        {cammute ? <CammutedFeed display={user.username} /> : <MyRTCView />}
 
-        {
-          question && (
-            <View style={styles.questionContainer}>
-              <Icon
-                name="question-mark"
-                size={40}
-                color="white"
-                style={styles.question}
-              />
-            </View>
-          )
-        }
+        {question && (
+          <View style={styles.questionContainer}>
+            <Icon
+              name="question-mark"
+              size={40}
+              color="white"
+              style={styles.question}
+            />
+          </View>
+        )}
       </View>
     </View>
   );
@@ -45,27 +38,27 @@ const MyRoomMedia = () => {
 export default MyRoomMedia;
 
 const styles = StyleSheet.create({
-  content          : {
-    aspectRatio    : 16 / 9,
-    alignItems     : 'center',
-    justifyContent : 'center',
-    backgroundColor: 'rgba(255,255,255,.1)'
+  content: {
+    aspectRatio: 16 / 9,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(255,255,255,.1)",
   },
-  overlay          : {
-    alignItems    : 'center',
-    justifyContent: 'center'
+  overlay: {
+    alignItems: "center",
+    justifyContent: "center",
   },
   questionContainer: {
-    flex          : 1,
-    position      : 'absolute',
-    alignItems    : 'center',
-    justifyContent: 'center'
+    flex: 1,
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  question         : {
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
-    padding        : 10,
-    borderRadius   : 10,
-    borderColor    : 'white',
-    borderWidth    : 2
-  }
+  question: {
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    padding: 10,
+    borderRadius: 10,
+    borderColor: "white",
+    borderWidth: 2,
+  },
 });
