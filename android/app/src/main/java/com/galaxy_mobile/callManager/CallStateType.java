f11db593003a06f2ddc3f688d0e8e3bd8dbe849e
davgur <davgur.ru@gmail.com>
2025-11-12 16:52:40 +0200

    Refactor Android project structure and update package names

---
+++ android/app/src/main/java/com/galaxy_mobile/callManager/CallStateType.java
@@ -0,0 +1,12 @@
+package com.galaxy_mobile.callManager;
+
+/**
+ * Enum representing different call states that can be tracked by the application.
+ */
+public enum CallStateType {
+    ON_START_CALL,    // When a call is initiated or received
+    ON_END_CALL,      // When a call ends
+    ON_RINGING,       // When the phone is ringing (incoming call)
+    ON_OFFHOOK,       // When a call is active
+    UNKNOWN           // Unknown or unhandled state
+}
---
+++ android/app/src/main/java/com/galaxy_mobile/permissions/PermissionsModule.java
@@ -0,0 +1,60 @@
+package com.galaxy_mobile.permissions;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.module.annotations.ReactModule;
+import android.util.Log;
+import com.galaxy_mobile.logger.GxyLogger;
+import com.galaxy_mobile.SendEventToClient;
+
+/**
+ * Module that serves as a bridge for permissions handling in React Native.
+ * This module is only used for adding event listener for permissions in React.
+ */
+@ReactModule(name = PermissionsModule.NAME)
+public class PermissionsModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "PermissionsModule";
+    private static final String TAG = "PermissionsModule";
+    private final ReactApplicationContext reactContext;
+    private static boolean isInitialized = false;
+
+    public PermissionsModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        GxyLogger.d(TAG, "Creating PermissionsModule");
+        this.reactContext = reactContext;
+    }
+
+    public void initializeAfterPermissions() {
+        GxyLogger.d(TAG, "Initializing PermissionsModule after permissions granted");
+        PermissionsModule.isInitialized = true;
+        GxyLogger.d(TAG, "PermissionsModule initialized: " + PermissionsModule.isInitialized);
+
+        // Send event to React Native that permissions are ready
+        try {
+            WritableMap params = Arguments.createMap();
+            params.putBoolean("allGranted", true);
+            SendEventToClient.sendEvent("permissionsStatus", params);
+        } catch (Exception e) {
+            GxyLogger.e(TAG, "Error sending permissions status event: " + e.getMessage(), e);
+        }
+    }
+
+    @ReactMethod
+    public void getPermissionStatus(Promise promise) {
+        GxyLogger.d(TAG, "Getting permission status: " + PermissionsModule.isInitialized);
+        promise.resolve(PermissionsModule.isInitialized);
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return NAME;
+    }
+}
