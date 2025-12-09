6a3cd0b448b76d44adfe33491baf220eff79ae4a
davgur <davgur.ru@gmail.com>
2025-12-09 11:01:49 +0200

    Refactor MoreBtn component to enhance modal toggle functionality

--- src/bottomBar/MoreBtn.js
+++ src/bottomBar/MoreBtn.js
@@ -1,24 +1,31 @@
 import * as React from 'react';
-import { Pressable, View } from 'react-native';
+import { Pressable } from 'react-native';
+
 import { ChatCounter } from '../chat/ChatCounter';
+import BottomBarIconWithText from '../settings/BottomBarIconWithTextAnimated';
 import { useUiActions } from '../zustand/uiActions';
 import { bottomBar } from './helper';
-import BottomBarIconWithText from '../settings/BottomBarIconWithTextAnimated';
 export const MoreBtn = () => {
-  const { toggleMoreModal, moreModal } = useUiActions();
+  const { toggleMoreModal, moreModal, toggleShowBars } = useUiActions();
+
+  const handlePress = () => {
+    toggleShowBars(false, !moreModal);
+    toggleMoreModal();
+  };
+
   return (
-    <Pressable onPress={toggleMoreModal} style={bottomBar.moreSelBtn}>
-        <BottomBarIconWithText
-          iconName={moreModal ? 'close' : 'more-vert'}
-          text="close"
-          extraStyle={
-            moreModal
-              ? ['toggle_on_alt2b', 'toggle_on_icon_alt2']
-              : ['toggle_off', 'toggle_off_icon']
-          }
-          showtext={false}
-        />
-        <ChatCounter />
+    <Pressable onPress={handlePress} style={bottomBar.moreSelBtn}>
+      <BottomBarIconWithText
+        iconName={moreModal ? 'close' : 'more-vert'}
+        text="close"
+        extraStyle={
+          moreModal
+            ? ['toggle_on_alt2b', 'toggle_on_icon_alt2']
+            : ['toggle_off', 'toggle_off_icon']
+        }
+        showtext={false}
+      />
+      <ChatCounter />
     </Pressable>
   );
 };
