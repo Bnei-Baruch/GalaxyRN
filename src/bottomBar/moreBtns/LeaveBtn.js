67be51d3c1fb02d10681a701c799aa48b4c9a700
davgur <davgur.ru@gmail.com>
2025-12-21 10:30:47 +0200

    Update iOS project settings in Info.plist and project.pbxproj
    - Added and updated usage descriptions for camera and microphone access in Info.plist for clarity on their purpose during room calls.
    - Adjusted NSExceptionDomains in Info.plist for improved security configuration.
    - Simplified OTHER_LDFLAGS in project.pbxproj for cleaner project settings.

--- src/bottomBar/moreBtns/LeaveBtn.js
+++ src/bottomBar/moreBtns/LeaveBtn.js
@@ -4,19 +4,21 @@ import { Pressable } from 'react-native';
 import BottomBarIconWithText from '../../settings/BottomBarIconWithTextAnimated';
 import { useInRoomStore } from '../../zustand/inRoom';
 import { bottomBar } from '../helper';
+
 export const LeaveBtn = () => {
   const { exitRoom } = useInRoomStore();
   const { t } = useTranslation();
   const handlePress = () => exitRoom();
+
   return (
     <Pressable onPress={handlePress} style={bottomBar.btn}>
-        <BottomBarIconWithText
-          iconName="logout"
-          text={t('bottomBar.leave')}
-          extraStyle={['rest_alt', 'rest_alt_icon']}
-          showtext={true}
-          direction={['horizontal','horizontal']}
-        />
+      <BottomBarIconWithText
+        iconName="logout"
+        text={t('bottomBar.leave')}
+        extraStyle={['rest_alt', 'rest_alt_icon']}
+        showtext={true}
+        direction={['horizontal', 'horizontal']}
+      />
     </Pressable>
   );
 };
