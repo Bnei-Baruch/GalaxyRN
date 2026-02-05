06178336d0e9892bbfeb730ab48add1bd7a05f6e
davgur <davgur.ru@gmail.com>
2026-02-05 11:39:48 +0200

    Call media switch (#44)
    * Update Babel configuration to conditionally load environment variables based on production status and clean up index.js by removing unnecessary comments.
    
    * Update Android build configuration and dependencies
    
    - Added resolution strategy to force androidx.browser:browser:1.8.0 in build.gradle.
    - Included androidx.media:media:1.4.0 dependency in app's build.gradle.
    - Removed unnecessary @RequiresApi annotations from AudioDeviceModule and AudioFocusManager classes.
    - Updated gradle.lockfile to reflect changes in dependency configurations.
    
    * Remove androidx.media:media:1.4.0 dependency from build.gradle and update gradle.lockfile to reflect changes in dependency configurations.
    
    * Remove unused WebRTC audio configuration from MainActivity to streamline code and improve maintainability.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
+const pkg = require('./package.json');
+
+const { isProduction = true } = !!pkg?.config;
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['module:react-native-dotenv'],
+  plugins: [
+    ['module:react-native-dotenv', { moduleName: '@env', path: isProduction ? '.env' : '.env.dev' }],
+  ],
 };
